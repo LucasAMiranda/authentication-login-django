6f64a364f64d65935631be72b95f6c3dc52e6f33
pedroaraujo2023 <pedroaraujo.bra.eng@gmail.com>
2024-04-09 08:21:42 -0300

    a label do campo foi mudado pra senha

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,23 +1,24 @@
 from django import forms
 from .models import Usuarios
 
-
 class CustomLoginForm(forms.ModelForm):
     class Meta:
         model = Usuarios
-        fields = ['cpf', 'password'] 
-
+        fields = ['cpf', 'password']
+        labels = {
+            'password': 'Senha'  # Define o rótulo do campo de senha como "Senha"
+        }
         widgets = {
             'password': forms.PasswordInput(),  # Para mascarar a entrada da senha no HTML
         }
-
-
 
 class CadastroForm(forms.ModelForm):
     class Meta:
         model = Usuarios
         fields = ['nome_aluno', 'cpf', 'password', 'escola', 'turma']
-
+        labels = {
+            'password': 'Senha'  # Define o rótulo do campo de senha como "Senha"
+        }
         widgets = {
             'password': forms.PasswordInput(),  # Para mascarar a entrada da senha no HTML
         }
